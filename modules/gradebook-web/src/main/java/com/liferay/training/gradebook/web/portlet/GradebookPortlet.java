e27b35af23fc5651d61820f117cca055219e39a1
Mauricio <mauricio1acp@hotmail.com>
2020-11-01 19:22:13 +0100

    Initial

---
+++ modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/GradebookPortlet.java
@@ -0,0 +1,32 @@
+package com.liferay.training.gradebook.web.portlet;
+
+import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author mauri
+ */
+@Component(
+		immediate = true,
+		property = {
+				"com.liferay.portlet.display-category=training",
+				"com.liferay.portlet.css-class-wrapper=gradebook-portlet",
+				"com.liferay.portlet.header-portlet-css=/css/main.css",
+				"com.liferay.portlet.instanceable=false",
+				"javax.portlet.display-name=Gradebook",
+				"javax.portlet.init-param.template-path=/",
+				"javax.portlet.init-param.view-template=/view.jsp",
+				"javax.portlet.name=" + GradebookPortletKeys.Gradebook,
+				"javax.portlet.resource-bundle=content.Language",
+				"javax.portlet.security-role-ref=power-user,user",
+				"javax.portlet.init-param.add-process-action-success-action=false"
+		},
+		service = Portlet.class
+)
+public class GradebookPortlet extends MVCPortlet {
+}
